2b7dabd171b18e0cfb110c19716eae4304c9fe66
Richard <programmingislife23@gmail.com>
2024-02-23 20:08:39 -0300

    exercitando conhecimentos em objetos, criando um objeto de clientes, adicionando nomes e interesses.

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+const clientes = {
+  interessesClientes: {},
+};
+console.log(clientes);
 function cadastrandoCliente() {
   let inputUsuario = prompt("Qual o seu nome? ");
-  const clientes = {
-    nomes: {},
+  let interesses = prompt("Qual o seu maior hobbie?");
+
+  // Adicionando nome e interesses ao objeto clientes.nomesIdadesClientes
+  clientes.interessesClientes[inputUsuario] = {
+    nome: inputUsuario,
+    interesses: interesses,
   };
+}
+
+programOn = true;
+
+while (programOn == true) {
+  cadastrandoCliente();
+  console.log(clientes);
+  let continuarEncerrar = prompt("encerrar/continuar");
+  if (continuarEncerrar == "encerrar") {
+    programOn = false;
+  }
 }
